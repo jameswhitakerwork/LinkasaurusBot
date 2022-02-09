483ef3bb79191cce5219f52333b05e8d13164442
James Whitaker <jameswhitakerwork@gmail.com>
2022-02-09 01:39:40 +0000

    init

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ async def on_ready():
   print("logged in as {0.user}".format(client))
 
 
-
 #Database balance adjustments
 def set_balance(username, balance):
   if "balances" in db.keys():
@@ -38,23 +37,32 @@ def reset_all_balances():
 def start_new_task(invite_link, no_invites):
   if "tasks" in db.keys():
     tasks = db["tasks"]
-    tasks[invite_link] = {"no_invites" : no_invites, "users" : []}
+    tasks[invite_link] = {"no_invites" : int(no_invites), "users" : []}
     db["tasks"] = tasks
   else:
-    db["tasks"] = {invite_link: {"no_invites" : no_invites, "users" : []}
+    db["tasks"] = {invite_link: {"no_invites" : int(no_invites), "us ers" : []}}
     print("tasks db initialized")
+
+def add_user_to_task(invite_link, user):
+  '''If tasks is in the DB, and the link has already been added, and number of current users is less than the number of current invites, add the user to the invite_link in the DB.''' 
+  if "tasks" in db.keys():
+    if invite_link in db["tasks"].keys():
+      if len(db["tasks"][invite_link]["users"]) < db["tasks"][invite_link]["no_invites"]:
+        db["tasks"][invite_link]["users"] = db["tasks"][invite_link]["users"] + [user]
     
-
-
-
-
 
 #Bot commands
 @bot.command()
 async def print_balances(message):
   if "balances" in db.keys():
-    for user in db["balances"]:
-      await message.channel.send(str(db["balances"])) 
+    await message.channel.send(str(db["balances"]))
+
+@bot.command()
+async def print_tasks(message):
+  if "tasks" in db.keys():
+    await message.channel.send(str(db["tasks"]))
+
+
 
 @bot.command()
 async def start(message):
@@ -70,10 +78,18 @@ async def reset_balances(message):
     await message.channel.send("All balances have been deleted.")
 
 
-    
+#Test commands
+@bot.command()
+async def parrot(message, arg):
+  await message.channel.send(arg)
 
+@bot.command()
+async def start_task(message, invite_link, no_invites):
+  start_new_task(invite_link, int(no_invites))
 
-
+@bot.command()
+async def add_user_task(message, invite_link):
+  add_user_to_task(invite_link, message.author)
 
 #Text commands
 @client.event
@@ -94,8 +110,5 @@ async def on_message(message):
 
   await bot.process_commands(message)
 
-    
-
 
 bot.run(discord_bot_token)
-
